3a3a27dc70dd9b11ecbd91dcf065107f13729fb9
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2021-09-19 15:14:39 -0400

    Update StorageFactory.sol
    Calling addPerson function from outside of simpleStorage contract

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -9,13 +9,18 @@ contract StorageFactory {
     SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
+        //new variable of type 'simpleStorage' object equals a new SimpleStorage contract with no parameters
         SimpleStorage simpleStorage = new SimpleStorage();
+        //push 'simpleStorage' to the SimpleStorage array
         simpleStorageArray.push(simpleStorage);
     }
 
+    //anytime you want to interact with a contract, you need the address and the ABI
     function storageFactoryStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         //Address
-        //Application Binary Interface
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStorage.addPerson("Angelee", _simpleStorageNumber);
+        //Application Binary Interface comes from the import above
     }
 
 }
